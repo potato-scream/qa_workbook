54b49cde7229aaa87bfbc3da31d49bc12356a9fb
PotatoScream <kr.englesson@gmail.com>
2025-01-27 22:11:34 +0000

    deleted holdBrowser configuration

--- src/test/java/github/EnterprizePageTest.java
+++ src/test/java/github/EnterprizePageTest.java
@@ -1,12 +1,10 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,16 +13,12 @@ public class EnterprizePageTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void  enterprizePageTest() {
         open("https://github.com");
-        $(byText("Solutions")).hover();
-       // какой из этих двух вариантов предпочтительнее? на 24 строке, 26 или 27?
-       // $$(".HeaderMenu-nav button").findBy(text("Solutions")).hover();
-       // $("nav.HeaderMenu-nav").$(byText("Solutions")).hover();
+        $("nav.HeaderMenu-nav").$(byText("Solutions")).hover();
         $("[href='https://github.com/enterprise']").click();
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
     }
