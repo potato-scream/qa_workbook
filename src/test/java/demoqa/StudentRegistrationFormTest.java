56ddc59b19eb311bb14f694d54502341eb6c44a6
PotatoScream <kr.englesson@gmail.com>
2025-03-07 13:29:19 +0000

    added allure steps

--- src/test/java/demoqa/StudentRegistrationFormTest.java
+++ src/test/java/demoqa/StudentRegistrationFormTest.java
@@ -5,6 +5,23 @@ import org.junit.jupiter.api.Test;
 import pages.StudentRegistrationPage;
 import utils.RandomUtils;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class StudentRegistrationFormTest extends TestBase {
     StudentRegistrationPage studentRegistrationPage = new StudentRegistrationPage();
@@ -12,33 +29,45 @@ public class StudentRegistrationFormTest extends TestBase {
     @Tag("demoqa")
     @Test
     void studentRegistrationFormTest() {
-        studentRegistrationPage.openPage()
-                .removeBanner()
-                .setFirstName(randomUtils.firstName)
-                .setLastName(randomUtils.lastName)
-                .setEmail(randomUtils.userEmail)
-                .setGender()
-                .setPhoneNumber(randomUtils.phoneNumber)
-                .setSubject()
-                .setDateOfBirth()
-                .setHobby()
-                .setPicture()
-                .setAddress(randomUtils.streetAddress)
-                .setState()
-                .setCity()
-                .pressSubmit()
-                .checkModalIsVisible();
+        step("Open form", () -> {
+            studentRegistrationPage.openPage();
+        });
+        step("Remove banner", () -> {
+            studentRegistrationPage.removeBanner();
+        });
+        step("Fill form", () -> {
+            studentRegistrationPage.setFirstName(randomUtils.firstName)
+                    .setLastName(randomUtils.lastName)
+                    .setEmail(randomUtils.userEmail)
+                    .setGender()
+                    .setPhoneNumber(randomUtils.phoneNumber)
+                    .setSubject()
+                    .setDateOfBirth()
+                    .setHobby()
+                    .setPicture()
+                    .setAddress(randomUtils.streetAddress)
+                    .setState()
+                    .setCity();
+        });
+        step("Press Submit", () -> {
+            studentRegistrationPage.pressSubmit();
+        });
+        step("Check modal is visible", () -> {
+            studentRegistrationPage.checkModalIsVisible();
+        });
 
-        studentRegistrationPage.checkResults("Student Name", randomUtils.firstName + " " + randomUtils.lastName)
-                .checkResults("Student Email", randomUtils.userEmail)
-                .checkGender()
-                .checkResults("Mobile", randomUtils.phoneNumber)
-                .checkDateOfBirth()
-                .checkSubject()
-                .checkHobby()
-                .checkPicture()
-                .checkResults("Address", randomUtils.streetAddress)
-                .checkStateAndCity();
+        step("Check results", () -> {
+            studentRegistrationPage.checkResults("Student Name", randomUtils.firstName + " " + randomUtils.lastName)
+                    .checkResults("Student Email", randomUtils.userEmail)
+                    .checkGender()
+                    .checkResults("Mobile", randomUtils.phoneNumber)
+                    .checkDateOfBirth()
+                    .checkSubject()
+                    .checkHobby()
+                    .checkPicture()
+                    .checkResults("Address", randomUtils.streetAddress)
+                    .checkStateAndCity();
+        });
     }
 
     @Test
--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -1,12 +1,55 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+
 }
